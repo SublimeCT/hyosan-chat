2fa3a813dada188fa81aa0b83273d33418d79f18
kuidi‮‮ <hellosc@qq.com>
2025-03-12 17:09:22 +0800

    feat: 更新测试数据 loading 状态

--- src/components/hyosan-chat-bubble-list.ts
+++ src/components/hyosan-chat-bubble-list.ts
@@ -81,6 +81,7 @@ export class HyosanChatBubbleList extends ShoelaceElement {
     .bubble {
       word-wrap: break-word;
       table {
+        display: block;
         width: 100%;
         border-collapse: collapse;
         margin: 16px 0;
