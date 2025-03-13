eec24e5d7b9481892d6ebc749f58641fe2a4e3e9
kuidi‮‮ <hellosc@qq.com>
2025-03-13 10:46:46 +0800

    chore: 在 lib 产物中增加 shoelace 的 themes 文件

--- vite.config.lib.ts
+++ vite.config.lib.ts
@@ -1,6 +1,7 @@
 import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
 	preview: {
@@ -14,6 +15,19 @@ export default defineConfig({
 	plugins: [
 		// 用于生成 `d.ts` 文件, refer https://github.com/qmhc/vite-plugin-dts/blob/HEAD/README.zh-CN.md
 		dts({ tsconfigPath: './tsconfig.lib.json' }),
+		// https://www.npmjs.com/package/vite-plugin-static-copy
+		viteStaticCopy({
+			targets: [
+				{
+					src: './node_modules/@shoelace-style/shoelace/cdn/themes/dark.css',
+					dest: 'sheets',
+				},
+				{
+					src: './node_modules/@shoelace-style/shoelace/cdn/themes/light.css',
+					dest: 'sheets',
+				},
+			],
+		}),
 	],
 	build: {
 		lib: {
